f10bf278f21eb8ef1713177a45e9a8fc2b3e9d0a
nicorreo87 <nicorreo87@gmail.com>
2017-09-11 17:10:04 -0300

    Se crean propiedades y métodos accesores de prestamo

--- Negocio/Prestamo.cs
+++ Negocio/Prestamo.cs
@@ -11,12 +11,131 @@ namespace Negocio
 {
     public class Prestamo
     {
-        private int id;
+        private int id; // Autonumerico. Es el número de prestamo
+        private Socio socio; // Es para guardar el ID de Socio
+        private string cedulasocio; //Pongo la cédula para facilitar la migración
+        private DateTime fecha;
+        private DateTime hora;
+        private double monteopedido;
+        private double tasa;
+        private int cantidadcuotas;
+        private double importecuota;
+        private int numeroPrestamoAnt;
+        private double montopedidoAnt;
+        private double amortizacionVencer;
+        private double interesesVencer;
+        private int cuotasPactadas;
+        private int cuotasPagadas;
+        private int cuotaAnt;
+        private double tasaanterior;
+        private int anulado;
 
         public int Id
         {
             get { return id; }
             set { id = value; }
+        }
+   
+        public Socio Socio
+        {
+            get { return socio; }
+            set { socio = value; }
+        }
+
+        public string Cedulasocio
+        {
+            get { return cedulasocio; }
+            set { cedulasocio = value; }
+        }
+    
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        
+        public DateTime Hora
+        {
+            get { return hora; }
+            set { hora = value; }
+        }
+
+        public double Monteopedido
+        {
+            get { return monteopedido; }
+            set { monteopedido = value; }
+        }
+
+        public double Tasa
+        {
+            get { return tasa; }
+            set { tasa = value; }
+        }
+
+        public int Cantidadcuotas
+        {
+            get { return cantidadcuotas; }
+            set { cantidadcuotas = value; }
+        }
+
+        public double Importecuota
+        {
+            get { return importecuota; }
+            set { importecuota = value; }
+        }
+
+        public int NumeroPrestamoAnt
+        {
+            get { return numeroPrestamoAnt; }
+            set { numeroPrestamoAnt = value; }
+        }
+
+        public double MontopedidoAnt
+        {
+            get { return montopedidoAnt; }
+            set { montopedidoAnt = value; }
+        }
+
+        public double AmortizacionVencer
+        {
+            get { return amortizacionVencer; }
+            set { amortizacionVencer = value; }
+        }
+
+        public double InteresesVencer
+        {
+            get { return interesesVencer; }
+            set { interesesVencer = value; }
+        }
+
+        public int CuotasPactadas
+        {
+            get { return cuotasPactadas; }
+            set { cuotasPactadas = value; }
+        }
+
+        public int CuotasPagadas
+        {
+            get { return cuotasPagadas; }
+            set { cuotasPagadas = value; }
+        }
+
+        public int CuotaAnt
+        {
+            get { return cuotaAnt; }
+            set { cuotaAnt = value; }
+        }
+
+        public double Tasaanterior
+        {
+            get { return tasaanterior; }
+            set { tasaanterior = value; }
+        }
+
+        public int Anulado
+        {
+            get { return anulado; }
+            set { anulado = value; }
         }
 
         public DateTime VtoPrimerCuota(DateTime Fecha)
