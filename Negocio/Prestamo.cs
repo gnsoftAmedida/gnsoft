b77696920a5989443512e05017680c35dbb95090
nicorreo87 <nicorreo87@gmail.com>
2017-09-11 16:42:28 -0300

    Se convierten las siguientes fórmulas: CalculoInteres, Pago_Mora, Cuota, AmortCuota, AmortVencer, IntVencer, VtoPto, VtoPrimerCuota

--- Negocio/Prestamo.cs
+++ Negocio/Prestamo.cs
@@ -6,6 +6,7 @@ using Persistencia;
 using System.Data;
 using Microsoft.VisualBasic;
 
+
 namespace Negocio
 {
     public class Prestamo
@@ -18,7 +19,153 @@ namespace Negocio
             set { id = value; }
         }
 
-     /*   public double Cuota(double tasa, string CantidadCuotas, double Capital)
+        public DateTime VtoPrimerCuota(DateTime Fecha)
+        {
+            DateTime VtoPrimerCuota;
+            DateTime FechaNueva;
+
+            FechaNueva = DateTime.Today.AddDays(15);
+
+            // VEEER porque en la original se llamaba a la función UltimoDiaMes que Leo no la pasó
+            VtoPrimerCuota = Convert.ToDateTime(DateTime.DaysInMonth(FechaNueva.Year, FechaNueva.Month) + "/" + FechaNueva.Month + "/" + FechaNueva.Year);
+              
+            return VtoPrimerCuota;
+        }
+
+        public DateTime VtoPto(DateTime Fecha)
+        {
+            DateTime VtoPto;
+            if ((Microsoft.VisualBasic.Conversion.Val(Fecha.Day) <= 10))
+            { //Fecha tope dentro del mes para cierre de presupuesto
+                if ((Microsoft.VisualBasic.Conversion.Val(Fecha.Month) != 2))
+                {
+                    VtoPto = Convert.ToDateTime("30" + "/" + Fecha.Month + "/" + Fecha.Year);
+                }
+                else
+                {
+                    VtoPto = Convert.ToDateTime("28" + "/" + Fecha.Month + "/" + Fecha.Year);
+                }
+            }
+
+            if ((Microsoft.VisualBasic.Conversion.Val(Fecha.Month) == 12))
+            { // Si mes es igual a 12 y día mayor a 10 debe cerrar 
+
+                VtoPto = Convert.ToDateTime("30" + "/01/" + (Fecha.Year + 1)); // El mes siguiente del año siguiente
+            }
+            else if ((Microsoft.VisualBasic.Conversion.Val(Fecha.Month) == 1))
+            {
+                VtoPto = Convert.ToDateTime("28" + "/02/" + (Fecha.Year));
+            }
+            else
+            {
+                VtoPto = Convert.ToDateTime("30" + "/" + (Fecha.Month + 1) + "/" + Fecha.Year);
+            }
+            return VtoPto;
+        }
+
+        public double IntVencer(double ImpCuota, int CantidadCuotas, int NroCuota, double AmoVencer)
+        {
+
+            double IntVencer;
+
+            if ((CantidadCuotas - NroCuota) == 0)
+            {
+                IntVencer = 0;
+            }
+            else
+            {
+                IntVencer = Convert.ToDouble(Strings.Format(((ImpCuota * (CantidadCuotas - NroCuota)) - AmoVencer), "##########.00"));
+            }
+            return IntVencer;
+        }
+
+        public double Pago_Mora(double Importe, string Presupuesto, double Xmora, string QueFecha)
+        {
+            double Pago_Mora;
+
+            // Importe es la base de cálculo que quedó debiendo
+            // Presupuesto es el mes en que no se le pudo descontar
+            // Xmora  es la tasa para cobro de mora seteado en parametros
+
+            string Mes; // * 2;  VEEERRR
+            string Año; //* 4;   VEEERRR
+            DateTime FechasDesde;
+            DateTime QueFechaResultado;
+            int CantidadDias;
+            double TasaDeCobro;
+
+            Mes = Microsoft.VisualBasic.Strings.Mid(Presupuesto, 1, 2);
+            Año = Microsoft.VisualBasic.Strings.Mid(Presupuesto, 4);
+            TasaDeCobro = 0;
+
+            if (Microsoft.VisualBasic.Conversion.Val(Mes) == 12)
+            {
+                FechasDesde = Convert.ToDateTime("01/01/" + (Microsoft.VisualBasic.Conversion.Val(Año) + 1));
+            }
+            else
+            {
+                FechasDesde = Convert.ToDateTime("01/" + (Microsoft.VisualBasic.Conversion.Val(Mes) + 1) + "/" + Año);
+            }
+
+            if (QueFecha != "")
+            {
+                QueFechaResultado = Convert.ToDateTime(QueFecha);
+            }
+
+            if (QueFecha == "") //cobranza por caja
+            {
+                TimeSpan ts = DateTime.Today - FechasDesde;
+                CantidadDias = ts.Days;
+            }
+            else
+            {
+                TimeSpan ts = Convert.ToDateTime(QueFecha) - FechasDesde;
+                CantidadDias = ts.Days;
+            }
+
+            // tasa = tasa + 100; //ejemp. 60 + 100 = 160
+            // tasa = tasa / 100; //ejemp. 160 / 100 = 1.60
+            // tasa = Math.Pow(tasa, (1 / 12)) - 1; //esta es la tasa mensual;
+
+            TasaDeCobro = Math.Pow(TasaDeCobro, CantidadDias / 360);
+            TasaDeCobro = TasaDeCobro - 1;
+            Pago_Mora = Importe * TasaDeCobro;
+
+            return Pago_Mora;
+        }
+
+        public double AmortVencer(double tasa, int CantidadCuotas, int NroCuota, double ImpCuota)
+        {
+            double AmortVencer;
+
+            tasa = tasa + 100; //ejemp. 60 + 100 = 160
+            tasa = tasa / 100; //ejemp. 160 / 100 = 1.60
+            tasa = Math.Pow(tasa, (1 / 12)) - 1; //esta es la tasa mensual;
+
+            if ((CantidadCuotas - NroCuota) == 0)
+            {
+                AmortVencer = 0;
+            }
+
+            AmortVencer = Convert.ToDouble(Strings.Format(ImpCuota * ((1 - Math.Pow(1 / (1 + (tasa)), (CantidadCuotas - NroCuota))) / tasa), "##########.00"));
+
+            return AmortVencer;
+        }
+
+        public double AmortCuota(double tasa, int NroCuota, int CantidadCuotas, double Capital)
+        {
+            double AmortCuota;
+
+            tasa = tasa + 100; //ejemp. 60 + 100 = 160
+            tasa = tasa / 100; //ejemp. 160 / 100 = 1.60
+            tasa = Math.Pow(tasa, (1 / 12)) - 1; //esta es la tasa mensual;
+
+            AmortCuota = Convert.ToDouble(Strings.Format(Financial.PPmt(tasa, NroCuota, CantidadCuotas, -Capital, 0, 0), "##########.00"));
+
+            return AmortCuota;
+        }
+
+        public double Cuota(double tasa, int CantidadCuotas, double Capital)
         {
             double Cuota;
 
@@ -26,10 +173,11 @@ namespace Negocio
             tasa = tasa / 100; //ejemp. 160 / 100 = 1.60
             tasa = Math.Pow(tasa, (1 / 12)) - 1; //esta es la tasa mensual;
 
-            Cuota = __Financial.
+            Cuota = Convert.ToDouble(Strings.Format(Financial.Pmt(tasa, CantidadCuotas, -Capital), "##########.00"));
 
+            return Cuota;
         }
-       */ 
+
         public double CalculoInteres(int Dias, int NroCuotas, double tasa, double Iva)
         {
             double Wiva;
@@ -56,12 +204,12 @@ namespace Negocio
                 CalculoInteres = Math.Pow(InteresMensual, Wdias);
                 CalculoInteres = CalculoInteres - 1;
                 CalculoInteres = (CalculoInteres / Wiva);
-                return Convert.ToDouble(CalculoInteres.ToString("##.#000000"));
+                return Convert.ToDouble(Strings.Format(CalculoInteres, "##.#000000"));
             }
             else
             {
                 CalculoInteres = ((InteresMensual - 1) / Wiva);
-                return Convert.ToDouble(CalculoInteres.ToString("##.#000000"));
+                return Convert.ToDouble(Strings.Format(CalculoInteres, "##.#000000"));
             }
         }
     }
