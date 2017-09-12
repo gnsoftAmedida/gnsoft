741379e42cf19419aae719eec38cbeb2ad7b659e
nicorreo87 <nicorreo87@gmail.com>
2017-09-12 18:04:07 -0300

    Se agrega tasa e iva a plan de préstamo. Se modifica tabla para futura migración.  Se agregan validaciones de que sean números y positivos. Se soluciona problema de punto y coma en la bd

--- COOPMEF/frmPlanDePrestamo.Designer.cs
+++ COOPMEF/frmPlanDePrestamo.Designer.cs
@@ -35,18 +35,22 @@
             this.btnEditarPlan = new System.Windows.Forms.Button();
             this.btnNuevoPlan = new System.Windows.Forms.Button();
             this.cmbPlan = new System.Windows.Forms.ComboBox();
-            this.texBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.texBoxNombrePlan = new System.Windows.Forms.TextBox();
             this.txtBoxCantCuotas = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtBoxInteres = new System.Windows.Forms.TextBox();
+            this.txtBoxTasaAnualSinIVA = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblDescripcion = new System.Windows.Forms.Label();
-            this.lblCantCuotas = new System.Windows.Forms.Label();
-            this.lblInteres = new System.Windows.Forms.Label();
             this.lblErrorGenerico = new System.Windows.Forms.Label();
+            this.lblTasaAnual = new System.Windows.Forms.Label();
+            this.lblCantCuotas = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtBoxIVA = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkVigencia = new System.Windows.Forms.CheckBox();
+            this.lblIva = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,10 +59,10 @@
             this.btnSalirPlan.Image = global::COOPMEF.Properties.Resources._1486109187_Log_Out;
             this.btnSalirPlan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnSalirPlan.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnSalirPlan.Location = new System.Drawing.Point(336, 234);
+            this.btnSalirPlan.Location = new System.Drawing.Point(333, 310);
             this.btnSalirPlan.Name = "btnSalirPlan";
             this.btnSalirPlan.Size = new System.Drawing.Size(96, 29);
-            this.btnSalirPlan.TabIndex = 35;
+            this.btnSalirPlan.TabIndex = 6;
             this.btnSalirPlan.Text = "     Salir";
             this.btnSalirPlan.UseVisualStyleBackColor = true;
             this.btnSalirPlan.Click += new System.EventHandler(this.btnSalirPlan_Click);
@@ -68,23 +72,24 @@
             this.btnCancelarPlan.Image = global::COOPMEF.Properties.Resources._1486109207_Cancel;
             this.btnCancelarPlan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnCancelarPlan.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnCancelarPlan.Location = new System.Drawing.Point(224, 233);
+            this.btnCancelarPlan.Location = new System.Drawing.Point(221, 309);
             this.btnCancelarPlan.Name = "btnCancelarPlan";
             this.btnCancelarPlan.Size = new System.Drawing.Size(96, 29);
-            this.btnCancelarPlan.TabIndex = 34;
+            this.btnCancelarPlan.TabIndex = 5;
             this.btnCancelarPlan.Text = "     Cancelar";
             this.btnCancelarPlan.UseVisualStyleBackColor = true;
             this.btnCancelarPlan.Click += new System.EventHandler(this.btnCancelarPlan_Click);
             // 
             // btnGuardarPlan
             // 
+            this.btnGuardarPlan.Enabled = false;
             this.btnGuardarPlan.Image = global::COOPMEF.Properties.Resources._1486108920_Save;
             this.btnGuardarPlan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnGuardarPlan.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnGuardarPlan.Location = new System.Drawing.Point(112, 233);
+            this.btnGuardarPlan.Location = new System.Drawing.Point(109, 309);
             this.btnGuardarPlan.Name = "btnGuardarPlan";
             this.btnGuardarPlan.Size = new System.Drawing.Size(96, 31);
-            this.btnGuardarPlan.TabIndex = 33;
+            this.btnGuardarPlan.TabIndex = 4;
             this.btnGuardarPlan.Text = "     Guardar";
             this.btnGuardarPlan.UseVisualStyleBackColor = true;
             this.btnGuardarPlan.Click += new System.EventHandler(this.btnGuardarPlan_Click);
@@ -97,7 +102,7 @@
             this.btnEliminarPlan.Location = new System.Drawing.Point(494, 148);
             this.btnEliminarPlan.Name = "btnEliminarPlan";
             this.btnEliminarPlan.Size = new System.Drawing.Size(96, 30);
-            this.btnEliminarPlan.TabIndex = 32;
+            this.btnEliminarPlan.TabIndex = 3;
             this.btnEliminarPlan.Text = "     Eliminar";
             this.btnEliminarPlan.UseVisualStyleBackColor = true;
             this.btnEliminarPlan.Click += new System.EventHandler(this.btnEliminarPlan_Click);
@@ -110,7 +115,7 @@
             this.btnEditarPlan.Location = new System.Drawing.Point(494, 97);
             this.btnEditarPlan.Name = "btnEditarPlan";
             this.btnEditarPlan.Size = new System.Drawing.Size(96, 30);
-            this.btnEditarPlan.TabIndex = 31;
+            this.btnEditarPlan.TabIndex = 2;
             this.btnEditarPlan.Text = "     Editar";
             this.btnEditarPlan.UseVisualStyleBackColor = true;
             this.btnEditarPlan.Click += new System.EventHandler(this.btnEditarPlan_Click);
@@ -123,7 +128,7 @@
             this.btnNuevoPlan.Location = new System.Drawing.Point(494, 46);
             this.btnNuevoPlan.Name = "btnNuevoPlan";
             this.btnNuevoPlan.Size = new System.Drawing.Size(96, 30);
-            this.btnNuevoPlan.TabIndex = 30;
+            this.btnNuevoPlan.TabIndex = 1;
             this.btnNuevoPlan.Text = "    Nuevo";
             this.btnNuevoPlan.UseVisualStyleBackColor = true;
             this.btnNuevoPlan.Click += new System.EventHandler(this.btnNuevoPlan_Click);
@@ -138,12 +143,12 @@
             this.cmbPlan.Text = "Seleccione un plan";
             this.cmbPlan.SelectedIndexChanged += new System.EventHandler(this.cmbPLan_SelectedIndexChanged);
             // 
-            // texBoxDescripcion
+            // texBoxNombrePlan
             // 
-            this.texBoxDescripcion.Location = new System.Drawing.Point(188, 62);
-            this.texBoxDescripcion.Name = "texBoxDescripcion";
-            this.texBoxDescripcion.Size = new System.Drawing.Size(132, 20);
-            this.texBoxDescripcion.TabIndex = 1;
+            this.texBoxNombrePlan.Location = new System.Drawing.Point(188, 62);
+            this.texBoxNombrePlan.Name = "texBoxNombrePlan";
+            this.texBoxNombrePlan.Size = new System.Drawing.Size(132, 20);
+            this.texBoxNombrePlan.TabIndex = 1;
             // 
             // txtBoxCantCuotas
             // 
@@ -164,67 +169,82 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(97, 111);
+            this.label2.Location = new System.Drawing.Point(62, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.Size = new System.Drawing.Size(100, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Cant Cuotas";
+            this.label2.Text = "Cantidad de Cuotas";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(97, 152);
+            this.label3.Location = new System.Drawing.Point(23, 155);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.Size = new System.Drawing.Size(139, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Interés";
+            this.label3.Text = "Tasa Anual Efectiva sin IVA";
             // 
-            // txtBoxInteres
+            // txtBoxTasaAnualSinIVA
             // 
-            this.txtBoxInteres.Location = new System.Drawing.Point(188, 152);
-            this.txtBoxInteres.Name = "txtBoxInteres";
-            this.txtBoxInteres.Size = new System.Drawing.Size(132, 20);
-            this.txtBoxInteres.TabIndex = 6;
+            this.txtBoxTasaAnualSinIVA.Location = new System.Drawing.Point(188, 152);
+            this.txtBoxTasaAnualSinIVA.Name = "txtBoxTasaAnualSinIVA";
+            this.txtBoxTasaAnualSinIVA.Size = new System.Drawing.Size(132, 20);
+            this.txtBoxTasaAnualSinIVA.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(97, 65);
+            this.label4.Location = new System.Drawing.Point(77, 68);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.Size = new System.Drawing.Size(85, 13);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Descripción";
+            this.label4.Text = "Nombre del Plan";
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.chkVigencia);
             this.panel1.Controls.Add(this.lblErrorGenerico);
-            this.panel1.Controls.Add(this.lblInteres);
+            this.panel1.Controls.Add(this.lblIva);
+            this.panel1.Controls.Add(this.lblTasaAnual);
             this.panel1.Controls.Add(this.lblCantCuotas);
-            this.panel1.Controls.Add(this.lblDescripcion);
+            this.panel1.Controls.Add(this.lblNombre);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.txtBoxInteres);
+            this.panel1.Controls.Add(this.txtBoxIVA);
+            this.panel1.Controls.Add(this.txtBoxTasaAnualSinIVA);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtBoxCantCuotas);
-            this.panel1.Controls.Add(this.texBoxDescripcion);
+            this.panel1.Controls.Add(this.texBoxNombrePlan);
             this.panel1.Controls.Add(this.cmbPlan);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(463, 215);
-            this.panel1.TabIndex = 36;
+            this.panel1.Size = new System.Drawing.Size(463, 292);
+            this.panel1.TabIndex = 0;
             // 
-            // lblDescripcion
+            // lblErrorGenerico
             // 
-            this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.ForeColor = System.Drawing.Color.Red;
-            this.lblDescripcion.Location = new System.Drawing.Point(327, 68);
-            this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(35, 13);
-            this.lblDescripcion.TabIndex = 8;
-            this.lblDescripcion.Text = "label5";
-            this.lblDescripcion.Visible = false;
+            this.lblErrorGenerico.AutoSize = true;
+            this.lblErrorGenerico.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorGenerico.Location = new System.Drawing.Point(185, 267);
+            this.lblErrorGenerico.Name = "lblErrorGenerico";
+            this.lblErrorGenerico.Size = new System.Drawing.Size(35, 13);
+            this.lblErrorGenerico.TabIndex = 11;
+            this.lblErrorGenerico.Text = "label5";
+            this.lblErrorGenerico.Visible = false;
+            // 
+            // lblTasaAnual
+            // 
+            this.lblTasaAnual.AutoSize = true;
+            this.lblTasaAnual.ForeColor = System.Drawing.Color.Red;
+            this.lblTasaAnual.Location = new System.Drawing.Point(330, 158);
+            this.lblTasaAnual.Name = "lblTasaAnual";
+            this.lblTasaAnual.Size = new System.Drawing.Size(35, 13);
+            this.lblTasaAnual.TabIndex = 10;
+            this.lblTasaAnual.Text = "label5";
+            this.lblTasaAnual.Visible = false;
             // 
             // lblCantCuotas
             // 
@@ -237,33 +257,61 @@
             this.lblCantCuotas.Text = "label5";
             this.lblCantCuotas.Visible = false;
             // 
-            // lblInteres
+            // lblNombre
             // 
-            this.lblInteres.AutoSize = true;
-            this.lblInteres.ForeColor = System.Drawing.Color.Red;
-            this.lblInteres.Location = new System.Drawing.Point(330, 158);
-            this.lblInteres.Name = "lblInteres";
-            this.lblInteres.Size = new System.Drawing.Size(35, 13);
-            this.lblInteres.TabIndex = 10;
-            this.lblInteres.Text = "label5";
-            this.lblInteres.Visible = false;
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.ForeColor = System.Drawing.Color.Red;
+            this.lblNombre.Location = new System.Drawing.Point(327, 68);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(35, 13);
+            this.lblNombre.TabIndex = 8;
+            this.lblNombre.Text = "label5";
+            this.lblNombre.Visible = false;
             // 
-            // lblErrorGenerico
+            // txtBoxIVA
             // 
-            this.lblErrorGenerico.AutoSize = true;
-            this.lblErrorGenerico.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorGenerico.Location = new System.Drawing.Point(185, 189);
-            this.lblErrorGenerico.Name = "lblErrorGenerico";
-            this.lblErrorGenerico.Size = new System.Drawing.Size(35, 13);
-            this.lblErrorGenerico.TabIndex = 11;
-            this.lblErrorGenerico.Text = "label5";
-            this.lblErrorGenerico.Visible = false;
+            this.txtBoxIVA.Location = new System.Drawing.Point(188, 191);
+            this.txtBoxIVA.Name = "txtBoxIVA";
+            this.txtBoxIVA.Size = new System.Drawing.Size(132, 20);
+            this.txtBoxIVA.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "% IVA a sumar a Tasa Anual";
+            // 
+            // chkVigencia
+            // 
+            this.chkVigencia.AutoSize = true;
+            this.chkVigencia.Checked = true;
+            this.chkVigencia.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkVigencia.Location = new System.Drawing.Point(188, 230);
+            this.chkVigencia.Name = "chkVigencia";
+            this.chkVigencia.Size = new System.Drawing.Size(108, 17);
+            this.chkVigencia.TabIndex = 12;
+            this.chkVigencia.Text = "Habilitar Vigencia";
+            this.chkVigencia.UseVisualStyleBackColor = true;
+            // 
+            // lblIva
+            // 
+            this.lblIva.AutoSize = true;
+            this.lblIva.ForeColor = System.Drawing.Color.Red;
+            this.lblIva.Location = new System.Drawing.Point(330, 194);
+            this.lblIva.Name = "lblIva";
+            this.lblIva.Size = new System.Drawing.Size(35, 13);
+            this.lblIva.TabIndex = 10;
+            this.lblIva.Text = "label5";
+            this.lblIva.Visible = false;
             // 
             // frmPlanDePrestamo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 274);
+            this.ClientSize = new System.Drawing.Size(602, 345);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnSalirPlan);
             this.Controls.Add(this.btnCancelarPlan);
@@ -289,17 +337,21 @@
         private System.Windows.Forms.Button btnEditarPlan;
         private System.Windows.Forms.Button btnNuevoPlan;
         private System.Windows.Forms.ComboBox cmbPlan;
-        private System.Windows.Forms.TextBox texBoxDescripcion;
+        private System.Windows.Forms.TextBox texBoxNombrePlan;
         private System.Windows.Forms.TextBox txtBoxCantCuotas;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtBoxInteres;
+        private System.Windows.Forms.TextBox txtBoxTasaAnualSinIVA;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblCantCuotas;
-        private System.Windows.Forms.Label lblDescripcion;
-        private System.Windows.Forms.Label lblInteres;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTasaAnual;
         private System.Windows.Forms.Label lblErrorGenerico;
+        private System.Windows.Forms.CheckBox chkVigencia;
+        private System.Windows.Forms.TextBox txtBoxIVA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblIva;
     }
 }
