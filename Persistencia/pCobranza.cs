d5c81abe1eaa4ef75e0530bc06a90eae5e5d8841
ginorov <ginoroviglio@gmail.com>
2017-10-11 22:03:09 -0300

    Se implementaron los métodos guardar, modificar y eliminar cobranzas y cobranzas_provisorias.

--- Persistencia/pCobranza.cs
+++ Persistencia/pCobranza.cs
@@ -11,7 +11,111 @@ using System.Collections;
 namespace Persistencia
 {
     public class pCobranza : CapaDatos
-    {
+    {   //método agregado el 11/10/17 por Gino
+        public void eliminarCobranza(int Id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+            string sql = "DELETE FROM cobranza WHERE cobranza_id = '" + Id + "'";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                    case 1451:
+                        //revisar siguiente línea  //  GINO
+                        MisExcepciones fk = new MisExcepciones("No se puede eliminar ya que exísten cobranzas pendientes ");
+                        throw fk;
+                }
+            }
+        }
+        //método agregado el 11/10/17 por Gino
+        public void modificarCobranza(int parCobranza_id, int parPrestamo_id, String parCedula, double parTasa, double parPorcentajeiva, double parMontopedido, int parCantidadcuotas, int parNrodecuotas, double parImportecuota, double parAmortizacioncuota, double parInteresCuota, double parIvaCuota, double parAmortizacionVencer, double parInteresVencer, double parAporteCapital, String parSocio_id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql = "Update cobranza set prestamo_id = '" + parPrestamo_id + "', cedula = '" + parCedula + "', tasa = '" + parTasa + "', porcentajeiva = '" + parPorcentajeiva + "', montopedido = '" + parMontopedido + "', cantidadcuotas = '" + parCantidadcuotas + "', nrodecuotas = '" + parNrodecuotas + "', importecuotas = '" + parImportecuota + "', AmortizacionCuota = '" + parAmortizacioncuota + "', InteresCuota = '" + parInteresCuota + "', IvaCuota = '" + parIvaCuota + "', AmortizacionVencer = '" + parAmortizacionVencer + "', InteresVencer = '" + parInteresVencer + "', aportecapital = '" + parAporteCapital + "', socio_id = '" + parSocio_id + "' WHERE cobranza_id =" + parCobranza_id;
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+
+                transaction.Commit();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+                throw ex;
+            }
+        }
+        //método agregado el 11/10/17 por Gino
+        public void GuardarCobranza(int parCobranza_id, int parPrestamo_id, String parCedula, double parTasa, double parPorcentajeiva, double parMontopedido, int parCantidadcuotas, int parNrodecuotas, double parImportecuota, double parAmortizacioncuota, double parInteresCuota, double parIvaCuota, double parAmortizacionVencer, double parInteresVencer, double parAporteCapital, String parSocio_id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql;
+            sql = "INSERT INTO cobranza (prestamo_id, cedula , tasa , porcentajeiva , montopedido, cantidadcuotas, nrodecuotas, importecuotas, AmortizacionCuota, InteresCuota , IvaCuota , AmortizacionVencer , InteresVencer , aportecapital, socio_id ) VALUES ('" + parPrestamo_id + "', '" + parCedula + "', '" + parTasa + "','" + parPorcentajeiva + "','" + parMontopedido + "', '" + parCantidadcuotas + "', '" + parNrodecuotas + "','" + parImportecuota + "','" + parAmortizacioncuota + "','" + parInteresCuota + "','" + parIvaCuota + "', '" + parAmortizacionVencer + "', '" + parInteresVencer + "','" + parAporteCapital + "','" + parSocio_id + "');" + "Select last_insert_id()";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                MySqlAdapter.InsertCommand = connection.CreateCommand();
+                MySqlAdapter.InsertCommand.Transaction = transaction;
+                MySqlAdapter.InsertCommand.CommandText = sql;
+                MySqlAdapter.InsertCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+            }
+
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                }
+            }
+        }
+
 
         public void eliminarAmortizacionVencerCero()
         {
--- Persistencia/pCobranzaProvisoria.cs
+++ Persistencia/pCobranzaProvisoria.cs
@@ -17,7 +17,7 @@ namespace Persistencia
             try
             {
                 MySqlConnection connection = conectar();
-
+                //faltaría agregar el parámetro aportecapital ya que fue agregado a la BD el 11/10 por Gino
                 MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
                 string sql = "SELECT cobranzaProvisoria_id, prestamo_id, cedula, tasa, porcentajeiva, montopedido, cantidadcuotas, nrodecuotas, importecuota, AmortizacionCuota, InteresCuota, IvaCuota,  AmortizacionVencer,InteresVencer, socio_id FROM cobranzaProvisoria";
                 DataSet ds = new DataSet();
@@ -35,6 +35,115 @@ namespace Persistencia
                 throw ex;
             }
         }
+
+        //método agregado el 11/10/17 por Gino
+        public void eliminarCobranzaProvisoria(int Id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+            string sql = "DELETE FROM cobranzaprovisoria WHERE cobranzaProvisoria_id = '" + Id + "'";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                    case 1451:
+                        //revisar siguiente línea  //  GINO
+                        MisExcepciones fk = new MisExcepciones("No se puede eliminar ya que exísten cobranzas pendientes ");
+                        throw fk;
+                }
+            }
+        }
+
+        //método agregado el 11/10/17 por Gino
+        public void modificarCobranzaProvisoria(int parCobranza_id, int parPrestamo_id, String parCedula, double parTasa, double parPorcentajeiva, double parMontopedido, int parCantidadcuotas, int parNrodecuotas, double parImportecuota, double parAmortizacioncuota, double parInteresCuota, double parIvaCuota, double parAmortizacionVencer, double parInteresVencer, double parAporteCapital, String parSocio_id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql = "Update cobranzaprovisoria set prestamo_id = '" + parPrestamo_id + "', cedula = '" + parCedula + "', tasa = '" + parTasa + "', porcentajeiva = '" + parPorcentajeiva + "', montopedido = '" + parMontopedido + "', cantidadcuotas = '" + parCantidadcuotas + "', nrodecuotas = '" + parNrodecuotas + "', importecuotas = '" + parImportecuota + "', AmortizacionCuota = '" + parAmortizacioncuota + "', InteresCuota = '" + parInteresCuota + "', IvaCuota = '" + parIvaCuota + "', AmortizacionVencer = '" + parAmortizacionVencer + "', InteresVencer = '" + parInteresVencer + "', aportecapital = '" + parAporteCapital + "', socio_id = '" + parSocio_id + "' WHERE cobranzaprovisoria_id =" + parCobranza_id;
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+
+                transaction.Commit();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+                throw ex;
+            }
+        }
+
+        //método agregado el 11/10/17 por Gino
+        public void GuardarCobranzaProvisoria(int parCobranza_id, int parPrestamo_id, String parCedula, double parTasa, double parPorcentajeiva, double parMontopedido, int parCantidadcuotas, int parNrodecuotas, double parImportecuota, double parAmortizacioncuota, double parInteresCuota, double parIvaCuota, double parAmortizacionVencer, double parInteresVencer, double parAporteCapital, String parSocio_id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql;
+            sql = "INSERT INTO cobranzaprovisoria (prestamo_id, cedula , tasa , porcentajeiva , montopedido, cantidadcuotas, nrodecuotas, importecuotas, AmortizacionCuota, InteresCuota , IvaCuota , AmortizacionVencer , InteresVencer , aportecapital, socio_id ) VALUES ('" + parPrestamo_id + "', '" + parCedula + "', '" + parTasa + "','" + parPorcentajeiva + "','" + parMontopedido + "', '" + parCantidadcuotas + "', '" + parNrodecuotas + "','" + parImportecuota + "','" + parAmortizacioncuota + "','" + parInteresCuota + "','" + parIvaCuota + "', '" + parAmortizacionVencer + "', '" + parInteresVencer + "','" + parAporteCapital + "','" + parSocio_id + "');" + "Select last_insert_id()";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                MySqlAdapter.InsertCommand = connection.CreateCommand();
+                MySqlAdapter.InsertCommand.Transaction = transaction;
+                MySqlAdapter.InsertCommand.CommandText = sql;
+                MySqlAdapter.InsertCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+            }
+
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                }
+            }
+        }
+
+
     }
 
 }
