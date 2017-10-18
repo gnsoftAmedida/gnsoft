12e3fada4894ac6eb694bf4ba7fc51d750455933
ginorov <ginoroviglio@gmail.com>
2017-10-17 21:15:24 -0300

    Se agregaron las calses excedidos y pExcedidos con los metodos insertar, modificar, eliminar y devolverTodos

---
+++ Negocio/Excedidos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistencia;
+using System.Data;
+
+namespace Negocio
+{
+    public class Excedidos
+    {
+        private int idExcedido;
+        private String presupuesto;
+        private String cedula;
+        private double aretener;
+        private double retenido;
+        private DateTime fechadepago;
+        private double importepagado;
+        private String presupuestodelpago;
+        private double aportecapital;
+
+        public int _idExcedido { get; set; }
+        public String _presupuesto { get; set; }
+        public String _cedula { get; set; }
+        public double _aretener { get; set; }
+        public double _retenido { get; set; }
+        public DateTime _fechadepago { get; set; }
+        public double _importepagado { get; set; }
+        public String _presupuestodelpago { get; set; }
+        public double _aportecapital { get; set; }
+
+        public DataSet devolverExcedidos()
+        {
+            pExcedidos tmpExcedido = new pExcedidos();
+            DataSet excedidos = tmpExcedido.devolverTodos();
+            return excedidos;
+        }
+
+        public void Guardar()
+        {
+            pExcedidos tmpExcedido = new pExcedidos();
+            tmpExcedido.GuardarExcedido(_presupuesto, _cedula, _aretener, _retenido, _fechadepago, _importepagado, _presupuestodelpago, _aportecapital);
+        }
+
+        public void modificarExcedido()
+        {
+            pExcedidos tmpExcedido = new pExcedidos();
+            tmpExcedido.modificarExcedido(_idExcedido,_presupuesto, _cedula, _aretener, _retenido, _fechadepago, _importepagado, _presupuestodelpago, _aportecapital);
+        }
+
+        public void eliminar()
+        {
+            pExcedidos tmpExcedido = new pExcedidos();
+            tmpExcedido.eliminarExcedido(this._idExcedido);
+        }  
+    }
+}
---
+++ Persistencia/pExcedidos.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Utilidades;
+using System.Data.OleDb;
+using System.Data;
+using System.Collections;
+
+namespace Persistencia
+{
+    public class pExcedidos : CapaDatos
+    {
+        public void eliminarExcedido(int Id)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+            string sql = "DELETE FROM excedidos WHERE excedido_id = '" + Id + "'";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                    
+                }
+            }
+        }
+
+        public void modificarExcedido(int idExcedido, String presupuesto, String cedula, double aretener, double retenido, DateTime fechadepago, double importepagado, String presupuestodelpago, double aportecapital)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql = "Update excedidos set presupuesto = '" + presupuesto + "', cedula = '" + cedula + "', aretener = '" + aretener + "', retenido = '" + retenido + "', fechadepago = '" + fechadepago + "', importepagado = '" + importepagado + "', presupuestodelpago = '" + presupuestodelpago + "', aportecapital = '" + aportecapital + "' WHERE excedido_id =" + idExcedido;
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+                MySqlAdapter.UpdateCommand = connection.CreateCommand();
+                MySqlAdapter.UpdateCommand.Transaction = transaction;
+
+                MySqlAdapter.UpdateCommand.CommandText = sql;
+                MySqlAdapter.UpdateCommand.ExecuteNonQuery();
+
+                transaction.Commit();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+                throw ex;
+            }
+        }
+
+        public void GuardarExcedido(String presupuesto, String cedula, double aretener, double retenido, DateTime fechadepago, double importepagado, String presupuestodelpago, double aportecapital)
+        {
+            MySqlConnection connection = conectar();
+            MySqlTransaction transaction = null;
+            MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+
+            string sql;
+            sql = "INSERT INTO excedidos (presupuesto,  cedula,  aretener,  retenido,  fechadepago,  importepagado,  presupuestodelpago,  aportecapital) VALUES ('" + presupuesto + "','" + cedula + "','" + aretener + "','" + retenido + "','" + fechadepago + "','" + importepagado + "','" + presupuestodelpago + "','" + aportecapital + "');" + "Select last_insert_id()";
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                MySqlAdapter.InsertCommand = connection.CreateCommand();
+                MySqlAdapter.InsertCommand.Transaction = transaction;
+                MySqlAdapter.InsertCommand.CommandText = sql;
+                MySqlAdapter.InsertCommand.ExecuteNonQuery();
+                transaction.Commit();
+                connection.Close();
+            }
+
+            catch (MySqlException ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+
+                switch (ex.Number)
+                {
+                    case 1406:
+                        MisExcepciones ep = new MisExcepciones("Datos muy largos");
+                        throw ep;
+                }
+            }
+        }
+
+        public DataSet devolverTodos()
+        {
+            try
+            {
+                MySqlConnection connection = conectar();
+
+                MySqlDataAdapter MySqlAdapter = new MySqlDataAdapter();
+                string sql = "SELECT * FROM excedidos";
+                DataSet ds = new DataSet();
+
+                connection.Open();
+                MySqlAdapter.SelectCommand = connection.CreateCommand();
+                MySqlAdapter.SelectCommand.CommandText = sql;
+                MySqlAdapter.Fill(ds, "excedidos");
+                connection.Close();
+                return ds;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
