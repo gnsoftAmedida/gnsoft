27235d8071aa756e26cd88e22a91843f15a3be35
ginorov <ginoroviglio@gmail.com>
2017-11-28 09:09:12 -0300

    Se agrega la pantalla Cancelación anticipada de prestamos

---
+++ COOPMEF/CancelacionAnticipadaDePrestmos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COOPMEF
+{
+    public partial class CancelacionAnticipadaDePrestmos : Form
+    {
+        public CancelacionAnticipadaDePrestmos()
+        {
+            InitializeComponent();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
